7cefdf6b254661c77e439872b88982e899959e92
Alamin <mdalaminhossain573190@gmail.com>
2024-05-07 14:34:47 +0600

    added delete booking

--- index.js
+++ index.js
@@ -43,6 +43,10 @@ async function run() {
     app.get('/services/:id', async(req ,res) =>{
         const id =req.params.id;
         const query ={_id: new ObjectId(id)}
+
+        const options ={
+          projection:{title : 1,price: 1,service_id: 1, img: 1},
+        }
         const result = await serviceCollection.findOne(query);
         res.send(result);
     })
@@ -50,8 +54,30 @@ async function run() {
 
 
     // booking
+
+    app.get('/bookings',async(req ,res) =>{
+      console.log(req.query.email)
+      let query ={};
+      if(req.query?.email){
+        query = {email:req.query.email}
+      }
+      const result = await bookingCollection.find(query).toArray()
+      res.send(result)
+    })
+
+
     app.post('/bookings',async(req,res) =>{
         const booking = req.body;
+        console.log(booking)
+        const result = await bookingCollection.insertOne(booking)
+        res.send(result);
+    })
+
+    app.delete('/booking/:id',async(req ,res) =>{
+      const id =req.params.id;
+      const query ={_id: new ObjectId(id)}
+      const result = await bookingCollection.deleteOne(query)
+      res.send(result)
     })
 
     // Send a ping to confirm a successful connection
